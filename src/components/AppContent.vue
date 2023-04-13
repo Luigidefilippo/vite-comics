626e439582f79aa41d088412eb12e8f8750d3933
Luigidefilippo <defilippoluigi65@gmail.com>
2023-04-13 18:16:30 +0200

    finsh section content goes here

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -5,7 +5,24 @@ export default {
 </script>
 
 <template>
-    <h2>Content</h2>
+    <div class="content-section">
+        <h2 class="content-text">Content goes here</h2>
+    </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+
+.content-section{
+    height: 100px;
+    width: 100%;
+    background-color: black;
+
+}
+.content-text{
+    color: white;
+    padding: 30px;
+}
+
+
+
+</style>
